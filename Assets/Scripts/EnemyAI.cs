e0be349111c06efd49fc52fe185aa75128444dc8
Xavier Faria <xavierfaria@outlook.com>
2021-02-24 02:36:03 -0500

    Initial Commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
